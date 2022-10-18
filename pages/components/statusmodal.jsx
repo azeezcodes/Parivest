1a6c0c553d632270954458fde04b170ea843c2ed
azeezcodes <81769914+azeezcodes@users.noreply.github.com>
2022-10-18 14:14:16 +0100

    added statusmodal to gitignore

--- pages/components/statusmodal.jsx
+++ pages/components/statusmodal.jsx
@@ -1,47 +1,45 @@
+// const StatusModal = () => {
+//    return (
+//       <div>
+//          <div>
+//             <p>Status</p> <div>p</div>
+//          </div>
+//          <div>
+//             <div>
+//                <select className={styles.selectall}>
+//                   <option value="">Denied</option>
+//                   <option value="">Approved</option>
+//                   <option value="">Pending</option>
+//                   <option value="">In-review</option>
+//                </select>
+//             </div>
+//             <div>
+//                <select className={styles.selectall}>
+//                   <option value="">Denied</option>
+//                   <option value="">Approved</option>
+//                   <option value="">Pending</option>
+//                   <option value="">In-review</option>
+//                </select>
+//             </div>
+//             <div>
+//                <select className={styles.selectall}>
+//                   <option value="">Denied</option>
+//                   <option value="">Approved</option>
+//                   <option value="">Pending</option>
+//                   <option value="">In-review</option>
+//                </select>
+//             </div>
+//             <div>
+//                <select className={styles.selectall}>
+//                   <option value="">Denied</option>
+//                   <option value="">Approved</option>
+//                   <option value="">Pending</option>
+//                   <option value="">In-review</option>
+//                </select>
+//             </div>
+//          </div>
+//       </div>
+//    );
+// };
 
-
-const StatusModal = () => {
-    return (
-       <div>
-          <div>
-             <p>Status</p> <div>p</div>
-          </div>
-          <div>
-             <div>
-                <select className={styles.selectall}>
-                   <option value="">Denied</option>
-                   <option value="">Approved</option>
-                   <option value="">Pending</option>
-                   <option value="">In-review</option>
-                </select>
-             </div>
-             <div>
-                <select className={styles.selectall}>
-                   <option value="">Denied</option>
-                   <option value="">Approved</option>
-                   <option value="">Pending</option>
-                   <option value="">In-review</option>
-                </select>
-             </div>
-             <div>
-                <select className={styles.selectall}>
-                   <option value="">Denied</option>
-                   <option value="">Approved</option>
-                   <option value="">Pending</option>
-                   <option value="">In-review</option>
-                </select>
-             </div>
-             <div>
-                <select className={styles.selectall}>
-                   <option value="">Denied</option>
-                   <option value="">Approved</option>
-                   <option value="">Pending</option>
-                   <option value="">In-review</option>
-                </select>
-             </div>
-          </div>
-       </div>
-    );
-}
- 
-export default StatusModal;
+// export default StatusModal;
